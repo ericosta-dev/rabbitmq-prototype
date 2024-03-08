2ee93646a4d8706901ce202dbf84f1e287f18d38
Eric Costa <ericcosta@MacBook.local>
2024-03-08 18:33:09 -0300

    feat: persisting new messages

--- app/utils/consumer.py
+++ app/utils/consumer.py
@@ -1,11 +1,19 @@
 import amqp
+import json
+from app.models import Messages
 
 class RabbitMQConsumer:
+    def persist_message(self, message):
+        msg_body = json.loads(message.body)
+        Messages.objects.create(body=msg_body)
+        print(f"Message persisted: {msg_body}")
+
     def start_consuming(self, connection_params, queue_name):
         with amqp.Connection(**connection_params) as c:
             ch = c.channel()
-            def on_message(message):
-                print('Received message (delivery tag: {}): {}'.format(message.delivery_tag, message.body))
-            ch.basic_consume(queue=queue_name, callback=on_message, no_ack=True)
+            ch.basic_consume(
+                queue=queue_name,
+                callback=self.persist_message,
+                no_ack=True)
             while True:
                 c.drain_events()
